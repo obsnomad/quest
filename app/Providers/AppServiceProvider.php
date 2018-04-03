e4a08d657aaf073616d8293aa863a1a5e7189eb2
akatelnikov <akatelnikov@corp.acesse.com>
2018-04-03 10:30:35 +0300

    Locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
             return \Auth::user()->hasPermissions(func_get_args(), 'or');
         });
 
-        setlocale(LC_ALL, 'ru_RU.utf8');
+        setlocale(LC_ALL, 'Russian');
         Carbon::setToStringFormat('d.m.Y H:i:s');
     }
 
